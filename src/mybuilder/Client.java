e182c173ee05f14e00f8c338b6500cbc0d3b98a8
DominatorMK <DominatorMK@DESKTOP-B0JP85S>
2018-05-13 15:23:08 +0200

    - zmodyfikowanie nazwy folderu mybyilderBad -> inhabitantInfo, - dodanie floderu inhabitantBulider.

--- src/mybuilder/Client.java
+++ src/mybuilder/Client.java
@@ -17,21 +17,21 @@ public class Client {
     public static void main(String[] args) {
         /* Bad */
         // Mieszaniec zarejestrowany na pobyt staly
-        mybuilder3Bad.Therapy therapy1 = new mybuilder3Bad.Therapy("Odpoczynek");
-        mybuilder3Bad.Diagnosis diagnosis1 = new mybuilder3Bad.Diagnosis("Lekkie zmęczenie");
-        mybuilder3Bad.AccommodationCard accommodationCard1 = new mybuilder3Bad.AccommodationCard("Pobyt stały");
+        inhabitantInfo.Therapy therapy1 = new inhabitantInfo.Therapy("Odpoczynek");
+        inhabitantInfo.Diagnosis diagnosis1 = new inhabitantInfo.Diagnosis("Lekkie zmęczenie");
+        inhabitantInfo.AccommodationCard accommodationCard1 = new inhabitantInfo.AccommodationCard("Pobyt stały");
 
-        mybuilder3Bad.Inhabitant inhabitant1 = new mybuilder3Bad.Inhabitant.Builder("Michal", 1, 2342234, 76, accommodationCard1)
+        inhabitantInfo.Inhabitant inhabitant1 = new inhabitantInfo.Inhabitant.Builder("Michal", 1, 2342234, 76, accommodationCard1)
                 .setDiagnosis(diagnosis1)
                 .setTherapy(therapy1)
                 .build();
 
         // Mieszkaniec zarejestrowany tymczasowo
-        mybuilder3Bad.Therapy therapy2 = new mybuilder3Bad.Therapy("Odpoczynek");
-        mybuilder3Bad.Diagnosis diagnosis2 = new mybuilder3Bad.Diagnosis("Lekkie zmęczenie");
-        mybuilder3Bad.AccommodationCard accommodationCard2 = new mybuilder3Bad.AccommodationCard("Pobyt tymczasowy");
+        inhabitantInfo.Therapy therapy2 = new inhabitantInfo.Therapy("Odpoczynek");
+        inhabitantInfo.Diagnosis diagnosis2 = new inhabitantInfo.Diagnosis("Lekkie zmęczenie");
+        inhabitantInfo.AccommodationCard accommodationCard2 = new inhabitantInfo.AccommodationCard("Pobyt tymczasowy");
 
-        mybuilder3Bad.Inhabitant inhabitant2 = new mybuilder3Bad.Inhabitant.Builder("Jacek", 1, 1442634, 88, accommodationCard2)
+        inhabitantInfo.Inhabitant inhabitant2 = new inhabitantInfo.Inhabitant.Builder("Jacek", 1, 1442634, 88, accommodationCard2)
                 .setDiagnosis(diagnosis2)
                 .setTherapy(therapy2)
                 .build();
