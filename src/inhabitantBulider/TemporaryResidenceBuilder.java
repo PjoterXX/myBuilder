33f2c0ead14f56873be9c1566ab779d02419ca51
DominatorMK <DominatorMK@DESKTOP-B0JP85S>
2018-05-13 15:32:11 +0200

    - implementacja interfejsu budowniczego dla klas mających na celu skonstruowanie i zestawienie części produktów w jeden główny produkt

--- src/inhabitantBulider/TemporaryResidenceBuilder.java
+++ src/inhabitantBulider/TemporaryResidenceBuilder.java
@@ -5,12 +5,39 @@
  */
 package inhabitantBulider;
 
+import inhabitantInfo.Inhabitant;
+
 
 /**
  *
  * @author LeopardProMK
  */
-public class TemporaryResidenceBuilder{
+public class TemporaryResidenceBuilder implements InhabitantBuilder {
+
+    @Override
+    public void buildAccommodationCard() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void buildTherapy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void buildDiagnosis() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void buildInhabitant() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Inhabitant getInhabitant() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
    
     
